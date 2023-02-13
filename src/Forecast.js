09626b3badb18d389381cdfdf238edf0b7113287
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2023-02-13 22:29:55 +0100

    Created markup and styles in Forecast section

---
+++ src/Forecast.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Forecast.css";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function Forecast() {
+  return (
+    <div className="Forecast">
+      <ul>
+        <li>
+          <h5>Fri</h5>
+          <ReactAnimatedWeather
+            icon="FOG"
+            color="grey"
+            size={48}
+            animate={true}
+          />
+          <div>15°C</div>
+        </li>
+        <li>
+          <h5>Sat</h5>
+          <ReactAnimatedWeather
+            icon="SNOW"
+            color="grey"
+            size={48}
+            animate={true}
+          />
+          <div>8°C</div>
+        </li>{" "}
+        <li>
+          <h5>Sun</h5>
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="grey"
+            size={48}
+            animate={true}
+          />
+          <div>12°C</div>
+        </li>{" "}
+        <li>
+          <h5>Mon</h5>
+          <ReactAnimatedWeather
+            icon="CLOUDY"
+            color="grey"
+            size={48}
+            animate={true}
+          />
+          <div>1°C</div>
+        </li>
+        <li>
+          <h5>Tue</h5>
+          <ReactAnimatedWeather
+            icon="WIND"
+            color="grey"
+            size={48}
+            animate={true}
+          />
+          <div>8°C</div>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchForm from "./SearchForm";
 import TodayWeather from "./TodayWeather";
+import Forecast from "./Forecast";
 
 import "./Main.css";
 
@@ -19,6 +20,7 @@ export default function Main() {
       </nav>
       <SearchForm />
       <TodayWeather />
+      <Forecast />
     </main>
   );
 }
