7ae423840a8c686c775a1a0c1ba29ce49d2077a5
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2023-02-13 22:53:04 +0100

    Change footer text; changed bg color

--- src/Footer.js
+++ src/Footer.js
@@ -2,27 +2,26 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return(<footer className="Footer">
-    <p>
-      This project was coded by{" "}
-      <a
-        href="https://www.linkedin.com/in/anastasiia-zavorotynska-849972133/"
-        target="_blank"
-        rel="noreferrer"
-        title="Link to Linkedin profile"
-      >
-        Anastasiia Zavorotynska
-      </a>{" "}
-      and it`s{" "}
-      <a
-        href="https://github.com/tinazavr/weather-react"
-        target="_blank"
-        rel="noreferrer"
-        title="Link to GitHub"
-      >
-        open-soursed
-      </a>{" "}
-      on GitHub
-    </p>
-  </footer>)
+  return (
+    <footer className="Footer">
+      <p>
+        <a
+          href="https://github.com/tinazavr/weather-react"
+          target="_blank"
+          rel="noreferrer"
+          title="Link to GitHub"
+        >
+          Open-source code
+        </a>, by{' '}
+        <a
+          href="https://www.linkedin.com/in/anastasiia-zavorotynska-849972133/"
+          target="_blank"
+          rel="noreferrer"
+          title="Link to Linkedin profile"
+        >
+          Anastasiia Zavorotynska
+        </a>
+      </p>
+    </footer>
+  );
 }
